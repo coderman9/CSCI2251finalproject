f5286da36b0d439bb88b0f8b85363e80fdd9e03e
benjamin <banker@cnm.edu>
2018-11-28 15:41:02 -0700

    clearer variable name

--- DatePair.java
+++ DatePair.java
@@ -2,7 +2,7 @@ import java.util.Date;
 public class DatePair extends Tuple<Date>
 {
     public final int ID;
-    public DatePair(Date start, Date end, int ID)
+    public DatePair(Date start, Date end, int tenantID)
     {
         super(start, end);
         this.ID=ID;
