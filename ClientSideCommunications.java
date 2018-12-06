1c92453b29997d07d9f86c89b5b04981060aab2d
Cesar Ornelas <ornelascesar234@gmail.com>
2018-12-05 20:37:51 -0700

    update to add function
    Added ctrl-c to close data base and client network shutdown

--- ClientSideCommunications.java
+++ ClientSideCommunications.java
@@ -1,132 +1,117 @@
 //package database;
 
-
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+// test function
 import java.util.Scanner;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 
 public class ClientSideCommunications {
-	
+
 	private InetAddress ip;
 	private Socket s;
-	
+
 	private ObjectOutputStream dos;
 	private ObjectInputStream dis;
-	
-	 public  ClientSideCommunications(InetAddress ad) throws UnknownHostException, IOException  
-	    { 
-		 System.out.println("client setup");
-	        
-	           
-	              
-	            // getting localhost ip 
-	             //ip = InetAddress.getByName("localhost"); 
-	      
-	            // establish the connection with server port 5056 
-	            s = new Socket(InetAddress.getLocalHost(), 5056);
-	            	      
-	            // obtaining input and out streams 
-	          
-	          
-	           
-	           dos = new ObjectOutputStream(s.getOutputStream());
-	           dos.flush();
-	           //dos.writeObject("hello");
-	           dis = new ObjectInputStream(s.getInputStream()); 
-	      
-	       
-	            
-	    } 
-	public String[] send(String[] tosend) throws IOException {
-		
-           
-            dos.writeObject(tosend); 
-              
-            // If client sends exit,close this connection  
-            // and then break from the while loop 
-            if(tosend[0].equals("Exit")) 
-            { 
-            	
-                System.out.println("Closing this connection : " + s); 
-                s.close(); 
-                System.out.println("Connection closed");
-                //scn.close(); 
-	            dis.close(); 
-	            dos.close();
-                
-            } 
-              
-            // printing date or time as requested by client
-            if(!tosend[0].equals("Exit")) {
-            String[] received = null;
-			try {
-				received = (String[]) dis.readObject();
-			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} 
-			System.out.print(received[0]);
-            return received;
-            }
-			return null;
-        } 
-	
 
-          
-	public static void main (String[] args) {
-		byte[] ipAddr = new byte[]{(byte) 192, (byte) 168, 0, 1};
-		ClientSideCommunications cl = null;
-		Scanner scn= new Scanner(System.in);
-		
-			InetAddress icup = null;
-			try {
-				 icup = InetAddress.getByAddress(ipAddr);
-			} catch (UnknownHostException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+	 public ClientSideCommunications(InetAddress ad) throws UnknownHostException, IOException
+	    {
+		 //System.out.println("client setup");
+
+	            // establish the connection with server port 5056
+				// connect to IP address
+	            s = new Socket(ad, 5056);
+	            // obtaining input and out streams
+	           dos = new ObjectOutputStream(s.getOutputStream());
+	           //dos.flush();
+	           dis = new ObjectInputStream(s.getInputStream());
+	    }
+	public ArrayList<String> send(ArrayList<String> tosend) throws IOException {
+		dos.flush();
+		dos.writeObject(tosend);
+      
+      ArrayList<String> received = new ArrayList<String>();
 			try {
 				
-				 cl =new ClientSideCommunications(icup);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
+				received = (ArrayList<String>) dis.readObject();
+				
+			} catch (ClassNotFoundException e) {
+				received.add("ClassNotFoundException");
 				e.printStackTrace();
+			}
+			//returns  ClassNotFoundException  if error was encountered
+			return received;
+      }
+
+			public void closeNetwork(){
+
+				//System.out.println("Closing this connection : " + s);
+				ArrayList<String> exit = new ArrayList<String>();
+				// send exit in a array list to tell server to disconnect.
+				exit.add("Exit");
+				try {
+					dos.writeObject(exit);
+					//dos.flush();
+					s.close();
+					dis.close();
+					dos.close();
+					// closes socket,input and output streams.
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+							
 			}
 			
-			while(true) {
-				
+			
+/* 
+				public static void main(String[]  args){
+		             ClientSideCommunications cl = null;
+					Scanner sc = new Scanner(System.in);
+					ArrayList<String> sending = new ArrayList<String>();
+					ArrayList<String> recived= new ArrayList<String>();
+					
+					
+					InetAddress myHost;
+					 try {
+				             myHost = InetAddress.getLocalHost();
+				             try {
+								cl = new ClientSideCommunications(myHost);
+							} catch (IOException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+				            System.out.println(myHost.getHostName());
+				        } catch (UnknownHostException ex) {
+				            ex.printStackTrace();
+				        }
+					 while(true) {
+						
+						 String input = sc.nextLine(); 
+						 sending=new ArrayList<String>();
+						 sending.add(input);
+						
+						 if(sending.get(0).equals("Exit")) {
+							 cl.closeNetwork();
+							 System.out.println("Closing");
+							 break;
+						 }
+						 try {
+								recived=cl.send(sending);
+							} catch (IOException e) {
+								e.printStackTrace();
+								break;
+							}
+						 
+						// System.out.println(sending);
+						 System.out.println(recived);
+			
+					 }
 
-                String get = scn.nextLine();
-                String[] tosend=new String[1];
-                tosend[0]=get;
-                try {
-					cl.send(tosend);
-					System.out.println(tosend[0]);
-					if(tosend[0].equals("Exit")) {
-						System.out.println("closing");
-						scn.close();
-						break;
-					}
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-					break;
-				}		
-			}
-				
-		
-		
-	}
-         
-   
-		 
+				}
+			*/
+
 }
-	 
-
-
--- ServerSideCommunications.java
+++ ServerSideCommunications.java
@@ -1,3 +1,5 @@
+//package database;
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -8,100 +10,131 @@ import java.util.ArrayList;
 
 
 public class ServerSideCommunications {
-	
-	 @SuppressWarnings("resource")
-	public static void main(String[] args) throws IOException  
-	    { 
-	        // server is listening on port 5056 
-		 ServerSocket ss = new ServerSocket(5056); 
-	          
-	        // running infinite loop for getting 
-	        // client request 
-	        System.out.println("gettign to loop");
-	        while (true)  
-	        { 
-	            Socket s = null; 
-	            System.out.println("got into loop");
-	              
-	            try 
-	            { 
-	                // socket object to receive incoming client requests 
-	                s = ss.accept(); 
-	                  
-	                System.out.println("A new client is connected : " + s); 
-	                  
-	                // obtaining input and out streams 
-	                ObjectInputStream dis = new ObjectInputStream(s.getInputStream()); 
+
+	 public static void main(String[] args) throws IOException
+	    {
+		 Runtime.getRuntime().addShutdownHook(new Thread() {
+		        public void run() {
+		            	System.out.println("closing Server");
+		            	//Database.Shutdown();
+		        }
+		    });
+	    
+	        // server is listening on port 5056
+		 ServerSocket ss = new ServerSocket(5056);
+
+	        // running infinite loop for getting
+	        // client request
+	        while (true)
+	        {
+	            Socket s = null;
+	            try
+	            {
+	                // socket object to receive incoming client requests
+	                s = ss.accept();
+
+	                System.out.println("A new client is connected : " + s);
+
+	                // obtaining input and out streams
+	                ObjectInputStream dis = new ObjectInputStream(s.getInputStream());
 	                ObjectOutputStream dos = new ObjectOutputStream(s.getOutputStream());
 	                dos.flush();
-	                  
-	                System.out.println("Assigning new thread for this client"); 
-	  
-	                // create a new thread object 
-	                Thread t = new ClientHandler(s, dis, dos); 
-	  
-	                // Invoking the start() method 
-	                t.start(); 
-	                  
-	            } 
-	            catch (Exception e){ 
-	                try {
-						s.close();
-					} catch (IOException e1) {
+
+	                System.out.println("Assigning new thread for this client");
+
+	                // create a new thread object
+	                Thread t = new ClientHandler(s, dis, dos);
+
+	                // Invoking the start() method
+	                t.start();
+
+	            }catch (Exception e){
+	           		try {
+	           			s.close();
+	           		} catch (IOException e1) {
 						e1.printStackTrace();
-					} 
-	                e.printStackTrace(); 
-	            } 
-	        } 
-		 
-	 }
-	       
-		 
-	  
+					}
+	                e.printStackTrace();
+	            }
+	        }
+	        
+	    }
 }
 
 
-class ClientHandler extends Thread  
-{ 
-     
-    final ObjectInputStream dis; 
-    final ObjectOutputStream dos; 
-    final Socket s; 
+class ClientHandler extends Thread
+{
+
+    final ObjectInputStream dis;
+    final ObjectOutputStream dos;
+    final Socket s;
     DataBase DB;
-    
-    
-      
-  
-    // Constructor 
-    public ClientHandler(Socket s, ObjectInputStream dis, ObjectOutputStream dos)  
-    { 
-        this.s = s; 
-        this.dis = dis; 
-        this.dos = dos; 
+
+
+
+
+    // Constructor
+    public ClientHandler(Socket s, ObjectInputStream dis, ObjectOutputStream dos)
+    {
+        this.s = s;
+        this.dis = dis;
+        this.dos = dos;
+        //creates new data base object for each thread.
         DB=new DataBase();
-    } 
-  
+    }
+
     @SuppressWarnings("unchecked")
 	@Override
-    public void run()  
-    { 
-    	ArrayList <String> received = new ArrayList <String> (); 
-       
-        while (true)  
-        { 
-            try { 
+    public void run()
+    {
+    	ArrayList <String> received = new ArrayList<String>();
+
+        while (true)
+        {
+            try {
                 try {
-					received = (ArrayList <String>) dis.readObject();
-				} catch (ClassNotFoundException e) {
-					e.printStackTrace();
-				} 
-                dos.writeObject(DB.handle(received));
-                
-                
-                
-            } catch (IOException e) { 
-                e.printStackTrace(); 
-            } 
-        } 
-    } 
-} 
+                				
+								received = (ArrayList <String>) dis.readObject();
+								
+								System.out.print("What we got:");
+								System.out.println(received);
+
+								} catch (ClassNotFoundException e) {
+									received.add("ClassNotFoundException");
+									e.printStackTrace();
+								}
+								if (received.get(0).equals("Exit")){
+									// exit once it received the Exit from input
+									System.out.println("Exit");
+									
+									dis.close();
+									dos.close();
+									s.close();
+									break;
+				}
+								// sends what it received to data base and send back
+								//database result
+								dos.writeObject(DB.handles(received));
+								//dos.writeObject(received);
+								
+            } catch (IOException e) {
+                e.printStackTrace();
+								//connection lost
+								// allows thread to exit since we don't want to reconnect
+								
+								try {
+									dis.close();
+									dos.close();
+									s.close();
+								} catch (IOException e1) {
+									// TODO Auto-generated catch block
+									e1.printStackTrace();
+								}
+								
+								break;
+            }
+            
+        }
+    }
+}
+
